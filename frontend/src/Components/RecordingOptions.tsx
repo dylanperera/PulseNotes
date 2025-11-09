1e93b0d00c0a1a633c4d9f29330cb124a363ec8a
Dylan Perera <d.perera@mail.utoronto.ca>
2025-11-09 18:07:41 -0500

    Added Resume and Pause button

--- frontend/src/Components/RecordingOptions.tsx
+++ frontend/src/Components/RecordingOptions.tsx
@@ -1,109 +1,29 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Menu, { MenuProps } from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
-import MicIcon from '@mui/icons-material/Mic';
-import { Mic } from 'lucide-react';
+import StartRecordingUI from './StartRecordingUI';
+import PauseUI from './PauseUI';
+import ResumeUI from './ResumeUI';
 
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: 'rgb(33, 63, 33)',
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-        ...theme.applyStyles('dark', {
-          color: 'inherit',
-        }),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-    ...theme.applyStyles('dark', {
-      color: theme.palette.grey[300],
-    }),
-  },
-}));
+type RecordingButtonProps = {
+  isRecording: boolean;
+  handleIsRecording: () => void;
+  recordingStarted: boolean,
+  handleStartRecording : () => void;
+};
 
-export default function RecordingOptions() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+export default function RecordingOptions({
+  isRecording,
+  handleIsRecording,
+  recordingStarted,
+  handleStartRecording
+}: RecordingButtonProps) {
 
-  return (
-    <div className="timer">
-      <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-        sx={{
-          backgroundColor: 'rgba(25,163,74,0.94)',
-          color: 'white',
-          '&:hover': {
-            backgroundColor: 'rgba(20, 130, 60, 0.94)',
-          },
-        }}
-      >
-      <MicIcon/> <b style={ {marginLeft: "8px"} }>Start Recording</b>
-      </Button>
-      <StyledMenu
-        id="demo-customized-menu"
-        slotProps={{
-          list: {
-            'aria-labelledby': 'demo-customized-button',
-          },
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-          Start Transcribing
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <p style={ {marginRight: "8px"} }>Upload Session Audio</p>
-          <FileUploadOutlinedIcon />
-        </MenuItem>
-      </StyledMenu>
-    </div>
-  );
+  if(recordingStarted === false)
+  {
+    return (<StartRecordingUI handleStartRecording={handleStartRecording} handleIsRecording={handleIsRecording}/>);
+  } else if (recordingStarted === true && isRecording === true) {
+    return (<PauseUI handleIsRecording={handleIsRecording}/>);
+  } else if (recordingStarted === true && isRecording === false) {
+    return (<ResumeUI handleIsRecording={handleIsRecording}/>);
+  } else {
+    return <div>Error</div>
+  }
 }
