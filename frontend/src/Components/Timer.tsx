41895dbb12aeacedab632a0c4b427111a81f80c8
Dylan Perera <d.perera@mail.utoronto.ca>
2025-11-12 11:04:59 -0500

    Formatted code

--- frontend/src/Components/Timer.tsx
+++ frontend/src/Components/Timer.tsx
@@ -1,51 +1,46 @@
-import { useState, useEffect, useMemo } from "react";
 import TimerOutlinedIcon from "@mui/icons-material/TimerOutlined";
+import { useEffect, useMemo, useState } from "react";
 import "../assets/styles/globals.css";
 
-export default function Timer(props: {isRecording: boolean, recordingStarted: boolean}) {
+export default function Timer(props: {
+	isRecording: boolean;
+	recordingStarted: boolean;
+}) {
+	const [time, setTime] = useState(0);
 
-	const [time, setTime] = useState(0); 
-	
 	useEffect(() => {
-
 		let interval: NodeJS.Timer | undefined;
 
-		if(props.isRecording)
-		{
-			interval = setInterval(() => {	
+		if (props.isRecording) {
+			interval = setInterval(() => {
 				// useEffect takes a "screen-shot" of the variables and uses that, so if we use something like (time + 1)
 				// it will use that same 'time' state value and since useEffect only changes on the dependency list (isRecording and recordingStarted)
 				// this means that it will stay the same untill we pause and resume again
-				setTime(prev => prev + 1);
+				setTime((prev) => prev + 1);
 			}, 1000);
+		} else if (props.isRecording === false && props.recordingStarted === true) {
+			clearInterval(interval);
 		}
-		else if (props.isRecording === false && props.recordingStarted === true)
-		{
-            clearInterval(interval);
-        }
 
 		// When the component unmounts we want to remove the interval, so it doesnt keep running
-        return () => {
-            clearInterval(interval);
-        };
-    }, [props.isRecording, props.recordingStarted]);
-
+		return () => {
+			clearInterval(interval);
+		};
+	}, [props.isRecording, props.recordingStarted]);
 
 	// Convert time from seconds to 00:00:00 format
 	const timeFormatted = useMemo(() => {
-
 		const hours = Math.floor(time / 3600);
 
 		const minutes = Math.floor((time % 3600) / 60);
 
 		const seconds = time % 60;
 
-		return (`${formatToTwoDigits(hours)}:${formatToTwoDigits(minutes)}:${formatToTwoDigits(seconds)}`);
-
+		return `${formatToTwoDigits(hours)}:${formatToTwoDigits(minutes)}:${formatToTwoDigits(seconds)}`;
 	}, [time]);
 
 	function formatToTwoDigits(num: number): string {
-		return num.toString().padStart(2, '0');
+		return num.toString().padStart(2, "0");
 	}
 
 	return (
