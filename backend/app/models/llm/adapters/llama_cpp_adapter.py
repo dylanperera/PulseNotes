071846a8367b508731f65d61facdd851991ccc5b
Dylan Perera <d.perera@mail.utoronto.ca>
2025-11-16 02:38:42 -0500

    Added initial infrastructure for LLM

---
+++ backend/app/models/llm/adapters/llama_cpp_adapter.py
@@ -0,0 +1,9 @@
+from base_llm_interface import ModelInterface
+
+class LlamaCppAdapter(ModelInterface):
+
+    def __init__(self):
+        pass
+
+    def generateSummary(self, prompt: str, input: str):
+        pass
