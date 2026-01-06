c8ce129de1f31ac78ffb771e1f9f36edcf210597
ferrerjayden <jaydenferrer123@gmail.com>
2026-01-06 17:47:31 -0500

    changes

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from backend.app.controllers.transcription_controller import TranscriptionController
+from backend.app.services.transcription_service import TranscriptionService
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from app.controllers.summarization_controller import SummarizationController
 
@@ -13,12 +15,26 @@ async def web_socket_endpoint(websocket: WebSocket):
         await websocket.accept()
 
         summarization_controller = SummarizationController(websocket)
+        transcription_service = TranscriptionService(
+            adapter=PyWhisperCppAdapter(model="small"),
+            asr_model="whispercpp"
+        )
+
+        transcription_controller = TranscriptionController(
+            websocket=websocket,
+            service=transcription_service
+        )
 
         while True:
             msg = await websocket.receive_json()
             msg_type = msg["type"]
 
             # add something for audio input
+            if msg_type == "start_transcription":
+                await transcription_controller.start()
+
+            elif msg_type == "stop_transcription":
+                await transcription_controller.stop()
 
             if msg_type == "transcription_chunk":
                 await summarization_controller.summarize_transcript(msg["payload"])
